01619240adaf5d777d58cca5bcf829a9474dac40
Henrique Carvalho <henricarvalho97@gmail.com>
2022-08-14 14:01:43 -0300

    Shell para criar servidor web com apache

---
+++ Linux/shell_para_criar_srv_web.sh
@@ -0,0 +1,32 @@
+
+###############################################################
+#Shell para criar vm e servidor web com Apache no Ubuntu      #
+# 				./Cria_SRV_Web.sh                     #
+#				chmod +x arquivo.sh                   #
+###############################################################
+
+echo "Buscando novos pacotes..."
+apt-get update
+
+echo "Atualizando os aplicativos..."
+apt-get upgrade -y
+
+echo "Instalando apache..."
+apt-get install Apache2 -y
+
+echo "Baixando pacote unzip..."
+apt-get install unzip -y
+
+cd /tmp
+
+echo "Baixando repositório..."
+wget https://github.com/denilsonbonatti/linux-site-dio/arquive/refs/heads/ma>
+
+echo "Descompactando..."
+unzip main.zip
+
+echo "Copiando arquivos..."
+cd linux-site-dio
+cp -R linux-site-dio-main /var/www/html
+
+echo "Finalizado!"
